d793ce58babd673543e22bbe215bd40f19f4c551
Daniel Golub <danielg6666@me.com>
2015-09-11 16:30:40 +0300

    POC

---
+++ testmodal.js
@@ -0,0 +1,14 @@
+/**
+ * UPStore Modal app
+ */
+
+var APP_ID = "55eabf3f9fb5ba02465c90a2";
+$("modal").each(function() {
+	$(this).html(" \
+		<div upstore-bind='title'></div> \
+		<div upstore-bind='div'></div> \
+	")
+	UPSTORE.retrieve(APP_ID, $(this).attr('upstore-arr-key'), this).then(function(response) {
+		console.info(response)
+	});
+})
