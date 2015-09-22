fc612a3fc8e3b63801f12a635025bb98b41b0a9e
Daniel Golub <danielg6666@me.com>
2015-09-22 13:10:13 +0300

    Revert "integration is now 2 lines"
    This reverts commit ed853b8088b60fd434ee677d55067c364018b03e.

--- testmodal.js
+++ testmodal.js
@@ -2,89 +2,121 @@
  * UPStore Modal app
  */
 
-(function(){
-	var APP_ID = "55eabf3f9fb5ba02465c90a2";
-	$("modal").each(function() {
-		$(this).html(' \
-			<div class="modal-background" upstore-style=\'{ bgColor: "background-color" }\'></div>\
-			<div class="modal-container" upstore-style=\'{ bgColor: "background-color", borderWidth: "border-width", borderStyle: "border-style", borderColor: "border-color" }\'>\
-				<div class="modal-title" upstore-bind="title" upstore-style=\'{ headerBg: "background-color" }\'></div> \
-				<div class="modal-close" upstore-style=\'{ headerBg: "color" }\' onclick="closeModal(this, false)">&#10006;</div> \
-				<div class="modal-content" upstore-bind="content"></div> \
-				<div style="width: 100%;text-align: center;">\
-					<button class="md-close" upstore-bind="actionText" onclick="closeModal(this)"></button>\
-				</div>\
+var APP_ID = "55eabf3f9fb5ba02465c90a2";
+$("modal").each(function() {
+	$(this).css("display", "none").html(' \
+		<div class="modal-background" upstore-style=\'{ bgColor: "background-color" }\'></div>\
+		<div class="modal-container" upstore-style=\'{ bgColor: "background-color", borderWidth: "border-width", borderStyle: "border-style", borderColor: "border-color" }\'>\
+			<div class="modal-title" upstore-bind="title" upstore-style=\'{ headerBg: "background-color" }\'></div> \
+			<div class="modal-close" upstore-style=\'{ headerBg: "color" }\' onclick="closeModal(this, false)">&#10006;</div> \
+			<div class="modal-content" upstore-bind="content"></div> \
+			<div style="width: 100%;text-align: center;">\
+				<button class="md-close" upstore-bind="actionText" onclick="closeModal(this)"></button>\
 			</div>\
-		')
+		</div>\
+	')
 
-		window.closeModal = function(button, openAction)
-		{
-			if(UPSTORE.showEditors)
-				return ;
-			openAction = (typeof(openAction) == 'undefined') ? true : openAction;
-			var modalToClose = $(button).parentsUntil('modal').parent();
-			modalToClose.css("opacity", "0")
-			setTimeout(function () {
-				modalToClose.css("display", "none")
-				if(openAction)
-				{
-					location.replace(window.UPSTORE.bindings[APP_ID][ARR_KEY].actionUrl);
-				}
-			}, 550);
-		}
-
-		// convert a hexidecimal color string to 0..255 R,G,B
-		hexToRGB = function(hex){
-		    var r = hex >> 16;
-		    var g = hex >> 8 & 0xFF;
-		    var b = hex & 0xFF;
-		    return [r,g,b];
-		}
-
-		function processColor(newVal)
-		{
-			if(newVal[0] == 0 && newVal[1] == 0 && newVal[2] == 0) {
-				$(modal).find(".modal-container .modal-title, .modal-container .modal-content").css("color", "#3d3d3d")
+	window.closeModal = function(button, openAction)
+	{
+		if(UPSTORE.showEditors)
+			return ;
+		openAction = (typeof(openAction) == 'undefined') ? true : openAction;
+		var modalToClose = $(button).parentsUntil('modal').parent();
+		modalToClose.css("opacity", "0")
+		setTimeout(function () {
+			modalToClose.css("display", "none")
+			if(openAction)
+			{
+				location.replace(window.UPSTORE.bindings[APP_ID][ARR_KEY].actionUrl);
 			}
-			else {
-				$(modal).find(".modal-container .modal-title, .modal-container .modal-content").css("color", "#fff")
-			}
-		}
-		var ARR_KEY = $(this).attr('upstore-arr-key'), modal = this;
-		UPSTORE.retrieve(APP_ID, ARR_KEY, this).then(function(response) {
-			var newVal;
-			Object.observe(UPSTORE.bindings[APP_ID][ARR_KEY], function(changes) {
-				if(changes[0].name == 'bgColor') {
-					newVal = hexToRGB(parseInt(changes[0].object.bgColor.replace("#", "")));
-					processColor(newVal);
-				}
-			});
-		});
+		}, 550);
+	}
 
-		// only on customization process
+	// convert a hexidecimal color string to 0..255 R,G,B
+	hexToRGB = function(hex){
+	    var r = hex >> 16;
+	    var g = hex >> 8 & 0xFF;
+	    var b = hex & 0xFF;
+	    return [r,g,b];
+	}
+
+	function processColor(newVal)
+	{
+		if(newVal[0] == 0 && newVal[1] == 0 && newVal[2] == 0) {
+			$(modal).find(".modal-container .modal-title, .modal-container .modal-content").css("color", "#3d3d3d")
+		}
+		else {
+			$(modal).find(".modal-container .modal-title, .modal-container .modal-content").css("color", "#fff")
+		}
+	}
+	var ARR_KEY = $(this).attr('upstore-arr-key'), modal = this;
+	UPSTORE.retrieve(APP_ID, ARR_KEY, this).then(function(response) {
+		var time = window.localStorage.getItem("_modal_time");
+		var currentTime = new Date();
+		currentTime = currentTime.getDate()+"/"+(currentTime.getMonth()+1)+"/"+currentTime.getFullYear();
 		if(UPSTORE.showEditors)
 		{
-			this.querySelector(".modal-title").setAttribute("contenteditable", true);
-			this.querySelector(".modal-title").addEventListener('keyup', function() {
-				window.UPSTORE.updateBinding(APP_ID, ARR_KEY, $(this).attr('upstore-bind'), $(this).html())
-			})
-			var script = document.createElement("script");
-			script.src = "https://tinymce.cachefly.net/4.2/tinymce.min.js"
-			script.onload = function() {
-				tinymce.init({
-				    selector: '.modal-content',
-				    inline: true,
-				    // toolbar: "undo redo",
-				    menubar: true,
-					setup: function(editor) {
-				        editor.on('blur', function(e) {
-				            window.UPSTORE.updateBinding(APP_ID, ARR_KEY, "content", tinyMCE.activeEditor.getContent());
-				        });
-				    },
-				});
-			};
-			document.body.appendChild(script);
-			$(this).find("button, .modal-close").attr("disabled", true).css("cursor", "not-allowed")
+			$(modal).css("display", "block")
+			localStorage.setItem("_modal_time", currentTime)			
 		}
-	})
-})();
+		else {
+			if(response.hold == 1) {
+				if(!time || time != currentTime) {
+					$(modal).css("display", "block")
+					localStorage.setItem("_modal_time", currentTime)
+				}
+			}
+			else if(response.hold == 2) {
+				var secondCheck = (currentTime.getDate()+1)+"/"+(currentTime.getMonth()+1)+"/"+currentTime.getFullYear();
+				if(!time || time != currentTime || time != secondCheck) {
+					$(modal).css("display", "block")
+					localStorage.setItem("_modal_time", currentTime)
+				}
+			}
+			else if(response.hold == 7) {
+				var explode = time.slice("/");
+				if(!time || explode[1] != (new Date().getMonth()+1)) {
+					$(modal).css("display", "block")
+					localStorage.setItem("_modal_time", currentTime)
+				}
+			}
+			else if(response.hold == 0 && !time) {
+				$(modal).css("display", "block")
+				localStorage.setItem("_modal_time", currentTime)
+			}
+		}
+		var newVal;
+		Object.observe(UPSTORE.bindings[APP_ID][ARR_KEY], function(changes) {
+			if(changes[0].name == 'bgColor') {
+				newVal = hexToRGB(parseInt(changes[0].object.bgColor.replace("#", "")));
+				processColor(newVal);
+			}
+		});
+	});
+
+	// only on customization process
+	if(UPSTORE.showEditors)
+	{
+		this.querySelector(".modal-title").setAttribute("contenteditable", true);
+		this.querySelector(".modal-title").addEventListener('keyup', function() {
+			window.UPSTORE.updateBinding(APP_ID, ARR_KEY, $(this).attr('upstore-bind'), $(this).html())
+		})
+		var script = document.createElement("script");
+		script.src = "https://tinymce.cachefly.net/4.2/tinymce.min.js"
+		script.onload = function() {
+			tinymce.init({
+			    selector: '.modal-content',
+			    inline: true,
+			    // toolbar: "undo redo",
+			    menubar: true,
+				setup: function(editor) {
+			        editor.on('blur', function(e) {
+			            window.UPSTORE.updateBinding(APP_ID, ARR_KEY, "content", tinyMCE.activeEditor.getContent());
+			        });
+			    },
+			});
+		};
+		document.body.appendChild(script);
+		$(this).find("button, .modal-close").attr("disabled", true).css("cursor", "not-allowed")
+	}
+})
