4895583e417becfdaa8c94bab92c69b3d87213fa
Daniel Golub <danielg6666@me.com>
2016-01-29 21:57:59 +0200

    production/staging separation

--- upstore.js
+++ upstore.js
@@ -2,327 +2,330 @@
  * UPStore front-end framework
  */
 
-window.UPSTORE = {
+(function() {
+	var UPSTORE_LANDSCAPE = window.location.href.indexOf("stagingapi.upstore.io") > -1 || document.querySelector("[upstore-framework]").getAttribute('src') == 'http://staging.upstore.io/upstore.js' ? 'STAGING' : 'PRODUCTION';
 
-	/**
-	 * Authentication details
-	 */
-	consumerKey: document.querySelector("[upstore-framework]").getAttribute("consumer-key"),
+	window.UPSTORE = {
 
-	/**
-	 * Show editors of UPP
-	 */
-	showEditors: false,
+		/**
+		 * Authentication details
+		 */
+		consumerKey: document.querySelector("[upstore-framework]").getAttribute("consumer-key"),
 
-	/**
-	 * Apps on current website
-	 */
-	upps: [],
+		/**
+		 * Show editors of UPP
+		 */
+		showEditors: false,
 
-	/**
-	 * URL of the server
-	 */
-	server: {
-		landscape: window.location.href.indexOf("stagingapi.upstore.io") > -1 || document.querySelector("[upstore-framework]").getAttribute('src') == 'http://staging.upstore.io/upstore.js' ? 'STAGING' : 'PRODUCTION',
-		base: this.landscape == 'STAGING' ? "http://stagingapi.upstore.io" : 'http://api.upstore.io',
-		azure: this.landscape == 'STAGING' ? "https://stagingupstore.blob.core.windows.net" : "https://upstore.blob.core.windows.net",
-		newUpp: "/upps/{appId}/{consumerKey}?arrKey={arrKey}",
-		retrieve: "/upps/{appId}?consumerKey={consumerKey}&arrKey={arrKey}",
-		sendMail: "/emails/upp",
-		upload: "/users/upload",
-	},
+		/**
+		 * Apps on current website
+		 */
+		upps: [],
 
-	/**
-	 * Get new app for current website
-	 */
-	newUpp: function(appId, arrKey)
-	{
-		// promise of new app when ready
-		return new Promise(function(resolve, error) {
-			var xmlHttp = new XMLHttpRequest(), url = UPSTORE.server.base+UPSTORE.server.newUpp.replace("{appId}", appId).replace("{arrKey}", arrKey).replace("{consumerKey}", UPSTORE.consumerKey);
-			xmlHttp.onreadystatechange = function() {
-				if (xmlHttp.readyState == 4) {
-					var json = JSON.parse(xmlHttp.responseText);
-					if(xmlHttp.status == 200) {
-						var element = document.createElement('script');
-						element.text = json.scripts;
-						document.body.appendChild(element);
+		/**
+		 * URL of the server
+		 */
+		server: {
+			base: UPSTORE_LANDSCAPE == 'STAGING' ? "http://stagingapi.upstore.io" : 'http://api.upstore.io',
+			azure: UPSTORE_LANDSCAPE == 'STAGING' ? "https://stagingupstore.blob.core.windows.net" : "https://upstore.blob.core.windows.net",
+			newUpp: "/upps/{appId}/{consumerKey}?arrKey={arrKey}",
+			retrieve: "/upps/{appId}?consumerKey={consumerKey}&arrKey={arrKey}",
+			sendMail: "/emails/upp",
+			upload: "/users/upload",
+		},
 
-						var element = document.createElement('style');
-						element.type = 'text/css';
-						if (element.styleSheet){
-						  element.styleSheet.cssText = json.styles;
-						} else {
-						  element.appendChild(document.createTextNode(json.styles));
-						}
-						document.head.appendChild(element);
-
-						resolve(json);
-					}
-					else
-					{
-						console.error('[UPSTORE] '+json.error)
-						error(xmlHttp.status, json);
-					}
-				}
-			};
-			xmlHttp.open("GET", url, true); // true for asynchronous
-			xmlHttp.send(null);
-		});
-	},
-
-	/**
-	 * Get content from URL, and append it as script
-	 * @param   {string}    source       URL
-	 * @param   {string}    section
-	 * @param   {object}    object
-	 */
-	getContent: function(source, object, section)
-	{
-		url = source
-		return new Promise(function(resolve, error) {
-			var xmlHttp = new XMLHttpRequest();
-			xmlHttp.onreadystatechange = function() {
-				if (xmlHttp.readyState == 4) {
-					if(xmlHttp.responseText) {
-						resolve({object: object, response: xmlHttp.responseText, section: section})
-					}
-					else
-						error(xmlHttp);
-				}
-			};
-			xmlHttp.open("GET", url, true); // true for asynchronous
-			xmlHttp.send(null);
-		})
-	},
-
-	/**
-	 * Retrieve properties of an app (that belongs to a user)
-	 */
-	retrieve: function(appId, arrKey, element)
-	{
-		if(UPSTORE.showEditors && document.querySelector("[upstore-loader]")) {
-			document.body.removeChild(document.querySelector("[upstore-loader]"));
-		}
-		arrKey = arrKey||"customize";
-		var url = UPSTORE.server.base+UPSTORE.server.retrieve.replace("{appId}", appId).replace("{consumerKey}", UPSTORE.consumerKey).replace("{arrKey}", arrKey);
-		return new Promise(function(resolve, error) {
-			var xmlHttp = new XMLHttpRequest();
-			xmlHttp.onreadystatechange = function() {
-				if (xmlHttp.readyState == 4) {
-					if(xmlHttp.responseText) {
+		/**
+		 * Get new app for current website
+		 */
+		newUpp: function(appId, arrKey)
+		{
+			// promise of new app when ready
+			return new Promise(function(resolve, error) {
+				var xmlHttp = new XMLHttpRequest(), url = UPSTORE.server.base+UPSTORE.server.newUpp.replace("{appId}", appId).replace("{arrKey}", arrKey).replace("{consumerKey}", UPSTORE.consumerKey);
+				xmlHttp.onreadystatechange = function() {
+					if (xmlHttp.readyState == 4) {
 						var json = JSON.parse(xmlHttp.responseText);
-						if(element)
-							UPSTORE.process(appId, arrKey, element, json)
-						resolve(json)
+						if(xmlHttp.status == 200) {
+							var element = document.createElement('script');
+							element.text = json.scripts;
+							document.body.appendChild(element);
+
+							var element = document.createElement('style');
+							element.type = 'text/css';
+							if (element.styleSheet){
+							  element.styleSheet.cssText = json.styles;
+							} else {
+							  element.appendChild(document.createTextNode(json.styles));
+							}
+							document.head.appendChild(element);
+
+							resolve(json);
+						}
+						else
+						{
+							console.error('[UPSTORE] '+json.error)
+							error(xmlHttp.status, json);
+						}
 					}
-					else
-						error(xmlHttp);
-				}
-			};
-			xmlHttp.open("GET", url, true); // true for asynchronous
-			xmlHttp.send(null);
-		})
-	},
+				};
+				xmlHttp.open("GET", url, true); // true for asynchronous
+				xmlHttp.send(null);
+			});
+		},
 
-	/**
-	 * Process the element's binding and options
-	 */
-	process: function(appId, arrKey, element, properties)
-	{
-		if(!window.UPSTORE.bindings[appId])
-	 		window.UPSTORE.bindings[appId] = {};
-		window.UPSTORE.bindings[appId][arrKey] = properties;
-		var key = "";
-		for(key in properties)
+		/**
+		 * Get content from URL, and append it as script
+		 * @param   {string}    source       URL
+		 * @param   {string}    section
+		 * @param   {object}    object
+		 */
+		getContent: function(source, object, section)
 		{
-			window.UPSTORE.updateBinding(appId, arrKey, key, properties[key])
-		}
-	},
+			url = source
+			return new Promise(function(resolve, error) {
+				var xmlHttp = new XMLHttpRequest();
+				xmlHttp.onreadystatechange = function() {
+					if (xmlHttp.readyState == 4) {
+						if(xmlHttp.responseText) {
+							resolve({object: object, response: xmlHttp.responseText, section: section})
+						}
+						else
+							error(xmlHttp);
+					}
+				};
+				xmlHttp.open("GET", url, true); // true for asynchronous
+				xmlHttp.send(null);
+			})
+		},
 
-	/**
-	 * Keep track of different UPSTORE key values
-	 */
-	bindings: {},
-
-	/**
-	 * Clean and organize dirty JSON
-	 */
-	dirtyJSON: function(text)
-	{
-		return JSON.parse(text.replace(/(['"])?([a-zA-Z0-9]+)(['"])?:/g, '"$2":'));
-	},
-
-	/**
-	 * Update a binding value
-	 * @param   {string}    appId
-	 * @param   {string}    arrKey
-	 * @param   {string}    key         Name of property
-	 * @param   {string}    value       New value of property
-	 */
-	updateBinding: function(appId, arrKey, key, value)
-	{
-		arrKey = arrKey||"customize";
-		// update the binding setting
-		window.UPSTORE.bindings[appId][arrKey][key] = value;
-
-		// first bind the text
-		var main = '[upstore-init="'+appId+'"][upstore-arr-key="'+arrKey+'"]';
-		var i, search = document.querySelectorAll(main+'[upstore-bind="'+key+'"], '+main+' [upstore-bind="'+key+'"]'), json = {};
-		for(i = 0;i<search.length;i++)
+		/**
+		 * Retrieve properties of an app (that belongs to a user)
+		 */
+		retrieve: function(appId, arrKey, element)
 		{
-			search[i].innerHTML = value;
-		}
+			if(UPSTORE.showEditors && document.querySelector("[upstore-loader]")) {
+				document.body.removeChild(document.querySelector("[upstore-loader]"));
+			}
+			arrKey = arrKey||"customize";
+			var url = UPSTORE.server.base+UPSTORE.server.retrieve.replace("{appId}", appId).replace("{consumerKey}", UPSTORE.consumerKey).replace("{arrKey}", arrKey);
+			return new Promise(function(resolve, error) {
+				var xmlHttp = new XMLHttpRequest();
+				xmlHttp.onreadystatechange = function() {
+					if (xmlHttp.readyState == 4) {
+						if(xmlHttp.responseText) {
+							var json = JSON.parse(xmlHttp.responseText);
+							if(element)
+								UPSTORE.process(appId, arrKey, element, json)
+							resolve(json)
+						}
+						else
+							error(xmlHttp);
+					}
+				};
+				xmlHttp.open("GET", url, true); // true for asynchronous
+				xmlHttp.send(null);
+			})
+		},
 
-		// then bind the remove from display
-		var main = '[upstore-init="'+appId+'"][upstore-arr-key="'+arrKey+'"]';
-		var i, search = document.querySelectorAll(main+'[upstore-if*="'+key+'"], '+main+' [upstore-if*="'+key+'"]'), json = {}, condition = "", passed = false;
-		for(i = 0;i<search.length;i++)
+		/**
+		 * Process the element's binding and options
+		 */
+		process: function(appId, arrKey, element, properties)
 		{
-			condition = search[i].getAttribute("upstore-if").replace(key, "");
-			passed = eval("value"+condition);
-			search[i].style.display = (passed ? 'block' : 'none');
-		}
-
-		// bind the styles
-		search = document.querySelectorAll(main+'[upstore-style*="'+key+'"], '+main+' [upstore-style*="'+key+'"]');
-		for(i = 0;i<search.length;i++)
-		{
-			json = window.UPSTORE.dirtyJSON(search[i].getAttribute('upstore-style'))
-			if(typeof(json[key]) != 'undefined')
+			if(!window.UPSTORE.bindings[appId])
+		 		window.UPSTORE.bindings[appId] = {};
+			window.UPSTORE.bindings[appId][arrKey] = properties;
+			var key = "";
+			for(key in properties)
 			{
-				if(typeof(json[key]) == 'string')
-				{
-					search[i].style[json[key]] = value;
-				}
-				else {
-					var b;
-					for(b = 0;b<json[key].length;b++)
-					{
-						search[i].style[json[key][b]] = value;
-					}
-				}
+				window.UPSTORE.updateBinding(appId, arrKey, key, properties[key])
 			}
-		}
+		},
 
-		// bind the classes
-		search = document.querySelectorAll(main+'[upstore-bind-class="'+key+'"], '+main+' [upstore-bind-class="'+key+'"]');
-		for(i = 0;i<search.length;i++)
-		{
-			var property = search[i].getAttribute('upstore-bind-class-old-value');
-			if(property != key)
-				search[i].className = search[i].className.replace(property, "");
-			// console.info(arguments)
-			search[i].className += " "+value;
-			search[i].setAttribute('upstore-bind-class-old-value', " "+value);
-		}
+		/**
+		 * Keep track of different UPSTORE key values
+		 */
+		bindings: {},
 
-		// bind the attributes
-		search = document.querySelectorAll(main+'[upstore-attr*="'+key+'"], '+main+' [upstore-attr*="'+key+'"]');
-		for(i = 0;i<search.length;i++)
+		/**
+		 * Clean and organize dirty JSON
+		 */
+		dirtyJSON: function(text)
 		{
-			json = window.UPSTORE.dirtyJSON(search[i].getAttribute('upstore-attr'))
-			if(typeof(json[key]) != 'undefined')
+			return JSON.parse(text.replace(/(['"])?([a-zA-Z0-9]+)(['"])?:/g, '"$2":'));
+		},
+
+		/**
+		 * Update a binding value
+		 * @param   {string}    appId
+		 * @param   {string}    arrKey
+		 * @param   {string}    key         Name of property
+		 * @param   {string}    value       New value of property
+		 */
+		updateBinding: function(appId, arrKey, key, value)
+		{
+			arrKey = arrKey||"customize";
+			// update the binding setting
+			window.UPSTORE.bindings[appId][arrKey][key] = value;
+
+			// first bind the text
+			var main = '[upstore-init="'+appId+'"][upstore-arr-key="'+arrKey+'"]';
+			var i, search = document.querySelectorAll(main+'[upstore-bind="'+key+'"], '+main+' [upstore-bind="'+key+'"]'), json = {};
+			for(i = 0;i<search.length;i++)
 			{
-				if(typeof(json[key]) == 'string')
+				search[i].innerHTML = value;
+			}
+
+			// then bind the remove from display
+			var main = '[upstore-init="'+appId+'"][upstore-arr-key="'+arrKey+'"]';
+			var i, search = document.querySelectorAll(main+'[upstore-if*="'+key+'"], '+main+' [upstore-if*="'+key+'"]'), json = {}, condition = "", passed = false;
+			for(i = 0;i<search.length;i++)
+			{
+				condition = search[i].getAttribute("upstore-if").replace(key, "");
+				passed = eval("value"+condition);
+				search[i].style.display = (passed ? 'block' : 'none');
+			}
+
+			// bind the styles
+			search = document.querySelectorAll(main+'[upstore-style*="'+key+'"], '+main+' [upstore-style*="'+key+'"]');
+			for(i = 0;i<search.length;i++)
+			{
+				json = window.UPSTORE.dirtyJSON(search[i].getAttribute('upstore-style'))
+				if(typeof(json[key]) != 'undefined')
 				{
-					search[i].setAttribute(json[key], value);
-				}
-				else {
-					var b;
-					for(b = 0;b<json[key].length;b++)
+					if(typeof(json[key]) == 'string')
 					{
-						search[i].setAttribute(json[key][b], value);
+						search[i].style[json[key]] = value;
+					}
+					else {
+						var b;
+						for(b = 0;b<json[key].length;b++)
+						{
+							search[i].style[json[key][b]] = value;
+						}
 					}
 				}
 			}
-		}
 
-		if(window.UPSTORE.registeredWatchers.length > 0)
+			// bind the classes
+			search = document.querySelectorAll(main+'[upstore-bind-class="'+key+'"], '+main+' [upstore-bind-class="'+key+'"]');
+			for(i = 0;i<search.length;i++)
+			{
+				var property = search[i].getAttribute('upstore-bind-class-old-value');
+				if(property != key)
+					search[i].className = search[i].className.replace(property, "");
+				// console.info(arguments)
+				search[i].className += " "+value;
+				search[i].setAttribute('upstore-bind-class-old-value', " "+value);
+			}
+
+			// bind the attributes
+			search = document.querySelectorAll(main+'[upstore-attr*="'+key+'"], '+main+' [upstore-attr*="'+key+'"]');
+			for(i = 0;i<search.length;i++)
+			{
+				json = window.UPSTORE.dirtyJSON(search[i].getAttribute('upstore-attr'))
+				if(typeof(json[key]) != 'undefined')
+				{
+					if(typeof(json[key]) == 'string')
+					{
+						search[i].setAttribute(json[key], value);
+					}
+					else {
+						var b;
+						for(b = 0;b<json[key].length;b++)
+						{
+							search[i].setAttribute(json[key][b], value);
+						}
+					}
+				}
+			}
+
+			if(window.UPSTORE.registeredWatchers.length > 0)
+			{
+				for(i = 0;i<window.UPSTORE.registeredWatchers.length;i++) {
+					watcher = window.UPSTORE.registeredWatchers[i];
+					if(watcher.appId == appId && watcher.arrKey == arrKey && (watcher.key == key || watcher.key == '*')) {
+						if(watcher.key == '*')
+							watcher.callback({
+								key: key,
+								value: value,
+							});
+						else
+							watcher.callback(value);
+					}
+				}
+			}
+		},
+
+		/**
+		 * Watch for binding updates
+		 * @param   {string}    appId
+		 * @param   {string}    arrKey
+		 * @param   {string}    key         Name of property
+		 * @param   {function}  callback    Function to be called
+		 */
+		watch: function(appId, arrKey, key, callback)
 		{
-			for(i = 0;i<window.UPSTORE.registeredWatchers.length;i++) {
-				watcher = window.UPSTORE.registeredWatchers[i];
-				if(watcher.appId == appId && watcher.arrKey == arrKey && (watcher.key == key || watcher.key == '*')) {
-					if(watcher.key == '*')
-						watcher.callback({
-							key: key,
-							value: value,
-						});
-					else
-						watcher.callback(value);
-				}
-			}
-		}
-	},
-
-	/**
-	 * Watch for binding updates
-	 * @param   {string}    appId
-	 * @param   {string}    arrKey
-	 * @param   {string}    key         Name of property
-	 * @param   {function}  callback    Function to be called
-	 */
-	watch: function(appId, arrKey, key, callback)
-	{
-		window.UPSTORE.registeredWatchers.push({
-			appId: appId,
-			arrKey: arrKey,
-			key: key,
-			callback: callback,
-		})
-	},
-
-	/**
-	 * Registered watchers
-	 */
-	registeredWatchers: [],
-
-
-	/**
-	 * Send an email on behalf of the UPP
-	 * @param   {string}    appId
-	 * @param   {string}    arrKey
-	 * @param   {string}    data        Data to be sent to the user
-	 * @returns	{object}	Promise
-	 */
-	sendMail: function(appId, arrKey, body) {
-		var url = UPSTORE.server.base+UPSTORE.server.sendMail;
-		return new Promise(function(resolve, error) {
-			var xmlHttp = new XMLHttpRequest();
-			xmlHttp.onreadystatechange = function() {
-				if (xmlHttp.readyState == 4) {
-					if(xmlHttp.responseText) {
-						resolve()
-					}
-					else
-						error(xmlHttp);
-				}
-			};
-
-			xmlHttp.open("PUT", url, true); // true for asynchronous
-			xmlHttp.setRequestHeader("Content-type", "application/json");
-			xmlHttp.send(JSON.stringify({
-				consumerKey: UPSTORE.consumerKey,
-				arrKey: arrKey,
+			window.UPSTORE.registeredWatchers.push({
 				appId: appId,
-				data: body
-			}));
-		})
-	}
-}
+				arrKey: arrKey,
+				key: key,
+				callback: callback,
+			})
+		},
 
-setTimeout(function () {
-	if(!window.UPSTORE.loaded) {
-		window.UPSTORE.loaded = true;
-		if(UPSTORE.consumerKey.length > 0) {
-			var toInit = document.querySelectorAll("[upstore-init]:not([upstore-init-called])"), i;
-			for(i = 0;i<toInit.length;++i)
-			{
-				UPSTORE.newUpp(toInit[i].getAttribute("upstore-init"), toInit[i].getAttribute("upstore-arr-key"));
+		/**
+		 * Registered watchers
+		 */
+		registeredWatchers: [],
+
+
+		/**
+		 * Send an email on behalf of the UPP
+		 * @param   {string}    appId
+		 * @param   {string}    arrKey
+		 * @param   {string}    data        Data to be sent to the user
+		 * @returns	{object}	Promise
+		 */
+		sendMail: function(appId, arrKey, body) {
+			var url = UPSTORE.server.base+UPSTORE.server.sendMail;
+			return new Promise(function(resolve, error) {
+				var xmlHttp = new XMLHttpRequest();
+				xmlHttp.onreadystatechange = function() {
+					if (xmlHttp.readyState == 4) {
+						if(xmlHttp.responseText) {
+							resolve()
+						}
+						else
+							error(xmlHttp);
+					}
+				};
+
+				xmlHttp.open("PUT", url, true); // true for asynchronous
+				xmlHttp.setRequestHeader("Content-type", "application/json");
+				xmlHttp.send(JSON.stringify({
+					consumerKey: UPSTORE.consumerKey,
+					arrKey: arrKey,
+					appId: appId,
+					data: body
+				}));
+			})
+		}
+	}
+
+	setTimeout(function () {
+		if(!window.UPSTORE.loaded) {
+			window.UPSTORE.loaded = true;
+			if(UPSTORE.consumerKey.length > 0) {
+				var toInit = document.querySelectorAll("[upstore-init]:not([upstore-init-called])"), i;
+				for(i = 0;i<toInit.length;++i)
+				{
+					UPSTORE.newUpp(toInit[i].getAttribute("upstore-init"), toInit[i].getAttribute("upstore-arr-key"));
+				}
+			}
+			else {
+				console.warn("[UPSTORE] PLEASE SPECIFY YOUR CONSUMER-KEY")
 			}
 		}
-		else {
-			console.warn("[UPSTORE] PLEASE SPECIFY YOUR CONSUMER-KEY")
-		}
-	}
-}, 0);
+	}, 0);
+})();
